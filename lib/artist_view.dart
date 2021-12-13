1267f50412af4275117dd1923ccec99be19a4386
melwinschultz <gusschulme@student.gu.se>
2021-12-13 11:37:39 +0100

    Album och Artist View

---
+++ lib/artist_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'album_lista.dart';
+import 'package:myapp/mina_reviews.dart';
+
+class artistView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("ALBUM"), actions: const [
+          IconButton(
+              icon: Icon(Icons.menu_outlined),
+              tooltip: 'menu',
+              onPressed: null),
+        ]),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 30,
+              ),
+              Container(
+                width: 250,
+                height: 250,
+                color: Colors.black,
+              ),
+              Container(height: 10),
+              const Text("AC/DC",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  )),
+              Container(height: 8),
+              const Text("info om artist och musik")
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.list),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MinaReviews()),
+              );
+            }));
+  }
+}
