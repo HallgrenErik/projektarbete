a9325af1c0c970fd0d03eb63679fb4b4a0b9c5b0
HallgrenErik <63341972+HallgrenErik@users.noreply.github.com>
2021-12-16 15:54:25 +0100

    Början på ett api
    Vi kan hämta data från ett api

--- lib/mina_reviews.dart
+++ lib/mina_reviews.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 import 'album_lista.dart';
+import 'api_album.dart';
 
 class MinaReviews extends StatelessWidget {
   const MinaReviews({Key? key}) : super(key: key);
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          reviewItem(
-              albumTitel: 'Tänd ett Ljus', artist: 'AcDc', albumBetyg: '5/5'),
-          review('detta är en review'),
-          const Divider(color: Colors.white),
-          reviewItem(
-            albumTitel: 'S/M',
-            artist: 'The Weekend',
-            albumBetyg: '2.7/5',
-          ),
-          review(
-              'detta är en reviewdetta är en reviewdetta är en reviewdetta är en reviewdetta är en reviews'),
-          const Divider(),
-        ],
-      ),
-    );
+        body: ListView(
+          children: [
+            reviewItem(
+                albumTitel: 'Tänd ett Ljus', artist: 'AcDc', albumBetyg: '5/5'),
+            review('detta är en review'),
+            const Divider(color: Colors.white),
+            reviewItem(
+              albumTitel: 'S/M',
+              artist: 'The Weekend',
+              albumBetyg: '2.7/5',
+            ),
+            review(
+                'detta är en reviewdetta är en reviewdetta är en reviewdetta är en reviewdetta är en reviews'),
+            const Divider(),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add, color: Colors.white70),
+            onPressed: () {
+              AlbumFetcher.getAlbum();
+            }));
   }
 }
