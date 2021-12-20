7cca15ed5601835c72049d55dffd44460549c5b7
HallgrenErik <63341972+HallgrenErik@users.noreply.github.com>
2021-12-20 09:48:29 +0100

    Ny api till album som fungerar

--- lib/api_album.dart
+++ lib/api_album.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 const API_URL =
-    'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=c995db7b4ae865f6a4243e90eddc9593&artist=Cher&album=Believe&format=json';
+    'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=$API_KEY&artist=Cher&album=Believe&format=json';
 String API_SOKURL =
     'http://ws.audioscrobbler.com/2.0/?method=album.search&album=abc&api_key=$API_KEY&format=json';
 const API_KEY = 'c995db7b4ae865f6a4243e90eddc9593';
@@ -13,8 +13,10 @@ class AlbumFetcher {
     http.Response response = await http.get(Uri.parse(API_URL));
     var jsonData = response.body;
     var obj = jsonDecode(jsonData);
-    print(obj['album'][0]);
-    return AlbumItem.fromJson(obj['album'][0]);
+    print(obj['album']['name']);
+    print(obj['album']['image'][4]['#text']);
+    print(obj['album']['wiki']['summary']);
+    return AlbumItem.fromJson(obj['album']);
   }
 
   static void getAlbum() async {
@@ -37,10 +39,10 @@ class AlbumItem {
   });
   static AlbumItem fromJson(Map<String, dynamic> json) {
     return AlbumItem(
-      albumTitel: json["strAlbum"],
-      artistName: json["strArtist"],
-      cover: json['strAlbumThumb'],
-      description: json['strDescriptionEN'],
+      albumTitel: json["name"],
+      artistName: json["artist"],
+      cover: json['image'][4]['#text'],
+      description: json['wiki']['summary'],
     );
   }
 }
