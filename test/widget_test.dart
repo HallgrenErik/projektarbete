c4e154ef7676c340065342e425e57249794cf577
melwinschultz <gusschulme@student.gu.se>
2022-01-06 10:41:20 +0100

    -

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:my_first_app/main.dart';
+import 'package:my_first_app/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
